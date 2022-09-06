c60b6647b0a6ed4dffb1e021739c789168ffc7f7
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-09-06 11:23:13 -0700

    Add test program

---
+++ src/TestSentryApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Sentry;
+
+namespace TestSentryApp;
+
+internal static class Program
+{
+    public static void Main()
+    {
+        using var _ = SentrySdk.Init(options =>
+        {
+            options.Dsn = "https://7c34571a56b94abe898bbd7721ac5f0c@o1162446.ingest.sentry.io/6723856";
+        });
+
+
+        throw new Exception("Test Exception");
+    }
+}
