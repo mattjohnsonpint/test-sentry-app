69ed9c0643bc48ffb8b08169b3b60e915364ba15
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-09-06 11:35:50 -0700

    Update Program.cs

--- src/TestSentryApp/Program.cs
+++ src/TestSentryApp/Program.cs
@@ -11,7 +11,11 @@ internal static class Program
             options.Dsn = "https://7c34571a56b94abe898bbd7721ac5f0c@o1162446.ingest.sentry.io/6723856";
         });
 
+        DoSomething();
+    }
 
+    private static void DoSomething()
+    {
         throw new Exception("Test Exception");
     }
 }
