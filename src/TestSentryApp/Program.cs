8554faac911bbd911b14b5b481cee17d0f25bd76
Matt Johnson-Pint <matt.johnson-pint@sentry.io>
2022-09-06 12:09:11 -0700

    2.0.0

--- src/TestSentryApp/Program.cs
+++ src/TestSentryApp/Program.cs
@@ -10,6 +10,7 @@ internal static class Program
         {
             options.Dsn = "https://7c34571a56b94abe898bbd7721ac5f0c@o1162446.ingest.sentry.io/6723856";
             options.AutoSessionTracking = true;
+            options.Release = "2.0.0";
         });
 
         DoSomething();
